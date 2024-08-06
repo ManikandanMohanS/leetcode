99ce67c29dee71e36ce3be64a0beafc781134060
ManikandanMohanS <manikandanmcse2023@ksrce.ac.in>
2024-08-06 21:30:37 +0530

    Time: 9 ms (6.65%), Space: 55.9 MB (45.84%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        
+        int fivedollar=0;
+        int tendollar=0;
+        int twentydollar=0;
+
+        for( int bill : bills){
+            int amount= bill-5;
+
+            int twentydollarneeded=Math.min(amount/20,twentydollar);
+            amount-=(twentydollarneeded*20);
+            twentydollar-=twentydollarneeded;
+
+            int tendollarneeded=Math.min(amount/10,tendollar);
+            amount-=(tendollarneeded*10);
+            tendollar-=tendollarneeded;
+
+             int fivedollarneeded=Math.min(amount/5,fivedollar);
+            amount-=(fivedollarneeded*5);
+            fivedollar-=fivedollarneeded;
+
+            if(amount > 0) return false;
+            if(bill==20) twentydollar+=1;
+            else if(bill==10) tendollar+=1;
+            else  fivedollar+=1;
+        }  
+
+        return true;           
+
+
+
+
+
+
+        
+    }
+}
