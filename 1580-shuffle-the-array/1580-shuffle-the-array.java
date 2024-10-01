eb28d853ffcb66b4a60248f9a57eb51e196a2e45
ManikandanMohanS <manikandanmcse2023@ksrce.ac.in>
2024-10-01 21:34:38 +0530

    Time: 0 ms (100%), Space: 45.2 MB (8.5%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+       int newarr[]=new  int[nums.length];
+    
+       for(int i=0;i<n;i++){
+        newarr[2*i]=nums[i];
+        newarr[(2*i)+1]= nums[n+i];
+       }
+    return newarr;
+    }
+}
