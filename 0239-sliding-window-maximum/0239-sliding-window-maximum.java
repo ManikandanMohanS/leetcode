1647ed311ba55931ba157a790f059c29f2e37a0e
ManikandanMohanS <manikandanmcse2023@ksrce.ac.in>
2025-02-07 10:30:16 +0530

    Time: 411 ms (5.52%), Space: 61.1 MB (31.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,32 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-          if (nums == null || nums.length == 0) return new int[0];
-
-        int n = nums.length;
-        int[] result = new int[n - k + 1]; // Result array
-        Deque<Integer> deque = new LinkedList<>(); // Deque to store indices
-
-        for (int i = 0; i < n; i++) {
-            // Remove elements that are out of the window (leftmost element)
-            if (!deque.isEmpty() && deque.peek() < i - k + 1) {
-                deque.poll();
-            }
-
-            // Remove all smaller elements from the back of the deque
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                deque.pollLast();
-            }
-
-            // Add current index to the deque
-            deque.offer(i);
-
-            // Store the maximum value in result once the first window is formed
-            if (i >= k - 1) {
-                result[i - k + 1] = nums[deque.peek()];
+           int i = 0, n = nums.length;
+        int[] maxs = new int[n - k + 1];
+        TreeMap<Integer, Integer> window = new TreeMap<>();
+        for (int j = 0; j < n; j++) {
+            window.put(nums[j], window.getOrDefault(nums[j], 0) + 1);
+            if (j >= k - 1) {
+                maxs[i] = window.lastKey();
+                int freqFirst = window.get(nums[i]);
+                if (freqFirst == 1) {
+                    window.remove(nums[i]);
+                } else {
+                    window.put(nums[i], freqFirst - 1);
+                }
+                i++;
             }
         }
-
-        return result;
+        return maxs;
 
     }
 }
