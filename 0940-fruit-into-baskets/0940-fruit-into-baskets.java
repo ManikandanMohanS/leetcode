10a21541d2517fc0549ff5f86b4765f299082699
ManikandanMohanS <manikandanmcse2023@ksrce.ac.in>
2025-02-14 21:20:14 +0530

    Time: 49 ms (76.6%), Space: 56.8 MB (9.75%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,17 +1,24 @@
+
+
 class Solution {
     public int totalFruit(int[] fruits) {
-        int maxFruits = 0, leftPtr = 0;
-        Map<Integer, Integer> diffTypesOfFruits = new HashMap<>();
-        for (int rightPtr = 0; rightPtr < fruits.length; rightPtr++) {
-            diffTypesOfFruits.put(fruits[rightPtr], diffTypesOfFruits.getOrDefault(fruits[rightPtr], 0) + 1);
-            while (diffTypesOfFruits.size() > 2) {
-                diffTypesOfFruits.put(fruits[leftPtr], diffTypesOfFruits.get(fruits[leftPtr]) - 1);
-                if (diffTypesOfFruits.get(fruits[leftPtr]) == 0)
-                    diffTypesOfFruits.remove(fruits[leftPtr]);
-                leftPtr++;
+        HashMap<Integer, Integer> fruitCount = new HashMap<>();
+        int left = 0, maxFruits = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            fruitCount.put(fruits[right], fruitCount.getOrDefault(fruits[right], 0) + 1);
+
+            while (fruitCount.size() > 2) { // If more than 2 types of fruit are in baskets
+                fruitCount.put(fruits[left], fruitCount.get(fruits[left]) - 1);
+                if (fruitCount.get(fruits[left]) == 0) {
+                    fruitCount.remove(fruits[left]);
+                }
+                left++; // Shrink the window
             }
-            maxFruits = Math.max(maxFruits, rightPtr - leftPtr + 1);
+
+            maxFruits = Math.max(maxFruits, right - left + 1); // Update max count
         }
+
         return maxFruits;
     }
 }
