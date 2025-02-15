9c8f3dbe2b85fd9376eabe55f81f78a40f9030e7
ManikandanMohanS <manikandanmcse2023@ksrce.ac.in>
2025-02-15 10:03:14 +0530

    Time: 3 ms (88.33%), Space: 46.7 MB (31.7%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -3,20 +3,20 @@ class Solution {
          int left = 0, maxLength = 0, zerosCount = 0;
 
         for (int right = 0; right < nums.length; right++) {
-            // If we encounter a 0, count it
+         
             if (nums[right] == 0) {
                 zerosCount++;
             }
 
-            // If zeros exceed k, move the left pointer to reduce zeros
+    
             while (zerosCount > k) {
                 if (nums[left] == 0) {
                     zerosCount--;
                 }
-                left++; // Shrink the window
+                left++; 
             }
 
-            // Update the maximum window size
+         
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
