32da621f6962580b83b2801ab3815fa803239fb0
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-16 09:59:04 +0530

    Time: 0 ms (100%), Space: 41.3 MB (30.35%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -3,19 +3,17 @@ class Solution {
          int n = stones.length;
         
         while (n > 1) {
-            // Sort stones in descending order
+           
             Arrays.sort(stones, 0, n);
-            
-            // Get the two heaviest stones
+  
             int stone1 = stones[n - 1];
             int stone2 = stones[n - 2];
 
-            // If they are not equal, replace the second largest stone
             if (stone1 != stone2) {
                 stones[n - 2] = stone1 - stone2;
-                n--; // Reduce the size of the array by 1
+                n--; 
             } else {
-                // Both stones are destroyed, reduce array size by 2
+               
                 n -= 2;
             }  
     }
