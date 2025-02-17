f69d5cbbe769a277bfa81e6b9fb024aa60624205
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-17 15:04:54 +0530

    Time: 2 ms (57.02%), Space: 40.8 MB (81.4%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
@@ -1,15 +1,15 @@
 class Solution {
     public int distributeCandies(int n, int limit) {
-           int count=0,sum=0;
-        for(int x=0;x<=limit;x++){
-            for( int y=0;y<=limit;y++){
-                for(int z=0;z<=limit;z++){
-                    sum=x+y+z;
-                    if(sum==n){
-                        count++;
-                    }
+        int count = 0;
+        
+        for (int x = 0; x <= limit; x++) {
+            for (int y = 0; y <= limit; y++) {
+                int j = n - (x + y);
+                if (j >= 0 && j <= limit) {
+                    count++;
                 }
             }
-        }return count;
+        }
+        return count;
     }
 }
