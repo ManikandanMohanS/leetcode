76346d9922866de639f2c72dfa3d11725f7a8b5a
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-21 10:32:28 +0530

    Time: 3 ms (78.44%), Space: 43.1 MB (93.7%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    private void findTargerSumcombo(int index, int arr[], int target, List<List<Integer>> ans, List<Integer> ds) {
+       
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+                   return; 
+            }
+         
+        for(int i=index;i<arr.length;i++){
+            if(i>index && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+
+            ds.add(arr[i]);
+            findTargerSumcombo(i+1,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+      
+    }
+
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+         List<List<Integer>> ans = new ArrayList<>();
+         Arrays.sort(candidates);
+        findTargerSumcombo(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+        
+    }
+}
