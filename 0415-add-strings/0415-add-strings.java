67d1ed5095afb9883ccf287baed3d78f2eb71e20
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-22 09:44:16 +0530

    Time: 1 ms (100%), Space: 42.2 MB (97.16%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        // Add digits from right to left
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            
+            int sum = digit1 + digit2 + carry;
+            result.append(sum % 10);  // Append the last digit of sum
+            carry = sum / 10;         // Update carry
+            
+            i--;
+            j--;
+        }
+
+        return result.reverse().toString(); // Reverse to get the correct order
+    }
+}
