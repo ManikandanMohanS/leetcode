bac8aed6e343d1a7ada8394fcd01c1ed535ecddf
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-22 11:20:46 +0530

    Time: 1 ms (92.92%), Space: 42.2 MB (65.41%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,12 +1,9 @@
 public class Solution {
-    public int titleToNumber(String s) {
-        if (s == null) return -1;
-        int sum = 0;
-        // for each loop so we don't need to mess with index values.
-        for (char c : s.toUpperCase().toCharArray()) {
-            sum *= 26;
-            sum += c - 'A' + 1;
+    public int titleToNumber(String columnTitle) {
+      int result=0;
+        for(char c:columnTitle.toCharArray()){
+            result=result*26+(c-'A'+1);
         }
-        return sum;
+        return result;
     }
 }
