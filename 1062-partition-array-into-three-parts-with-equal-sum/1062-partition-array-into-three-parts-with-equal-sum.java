9e1f674f730eff37e5303766443086ae1dd27677
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-24 21:57:07 +0530

    Time: 1 ms (100%), Space: 53.4 MB (85.89%) - LeetHub

--- 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.java
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.java
@@ -1,37 +1,28 @@
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
-        int totalSum = 0;
-
-        // Step 1: Compute total sum of array
-        for (int num : arr) {
-            totalSum += num;
+      int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
         }
+       
+        if(sum%3!=0){
+              return false;
+       }
+       int equalSum=sum/3;
+        sum=0;
+      int  equalCount=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+            if(sum==equalSum){
+                   equalCount++;
+                   sum=0;
+            
+                if(equalCount==2 && i<arr.length-1 ){
+                   return true;}
+                   
+           }
+    }
+return false;
 
-        // Step 2: Check if total sum is divisible by 3
-        if (totalSum % 3 != 0) {
-            return false;
-        }
-
-        int targetSum = totalSum / 3;  // Each partition should sum to this value
-        int currentSum = 0;
-        int partitionCount = 0;
-
-        // Step 3: Iterate through the array and find partitions
-        for (int i = 0; i < arr.length; i++) {
-            currentSum += arr[i];
-
-            // If we found a partition, reset sum and increment count
-            if (currentSum == targetSum) {
-                partitionCount++;
-                currentSum = 0;
-
-                // We need exactly three partitions
-                if (partitionCount == 2 && i < arr.length - 1) {
-                    return true;
-                }
-            }
-        }
-
-        return false; // If we couldn't find three valid partitions
     }
 }
