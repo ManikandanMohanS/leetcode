e255e1d5d2f7c37a521a237a3c4ea4d45275582e
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-24 20:57:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (54.28%) - LeetHub

--- 1983-maximum-population-year/1983-maximum-population-year.java
+++ 1983-maximum-population-year/1983-maximum-population-year.java
@@ -2,8 +2,7 @@ class Solution {
     public int maximumPopulation(int[][] logs) {
         
         int[] year = new int[2051];
-        
-		// O(n) -> n is log.length
+    
 		
         for(int[] log : logs){
             
@@ -13,11 +12,10 @@ class Solution {
        // System.out.println(Arrays.toString(year));
         int maxNum = year[1950], maxYear = 1950;
         
-		// O(100) -> 2050 - 1950 = 100
+	
 
         for(int i = 1951; i < year.length; i++){
-            year[i] += year[i - 1];  // Generating Prefix Sum
-            
+            year[i] += year[i - 1];  
             if(year[i] > maxNum){
                 maxNum = year[i];
                 maxYear = i;
