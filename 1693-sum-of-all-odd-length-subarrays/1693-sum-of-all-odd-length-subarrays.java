5ef77769b84013c5cfa4c01fe3f1764ec45c7527
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-02-24 21:16:00 +0530

    Time: 3 ms (23.63%), Space: 41.8 MB (16.96%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,8 +1,21 @@
+// class Solution {
+//     public int sumOddLengthSubarrays(int[] arr) {
+//         int sum = 0;
+//         for (int i = 0; i < arr.length; i++)
+//             sum += (arr[i] * ((((i + 1) * (arr.length - i)) + 1) / 2));
+//         return sum;
+//     }
+// }
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++)
-            sum += (arr[i] * ((((i + 1) * (arr.length - i)) + 1) / 2));
+        int sum=0;
+        for(int size=1;size<=arr.length;size+=2){
+            for(int i=0;i<=arr.length-size;i++){
+                for(int j=i;j<i+size;j++){
+                    sum += arr[j];
+                }
+            }
+        }
         return sum;
     }
 }
