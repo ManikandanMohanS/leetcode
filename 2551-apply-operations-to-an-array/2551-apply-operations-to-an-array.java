252834d553bb8103a52d58912fb2132e61f6360a
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-03-01 23:41:22 +0530

    Time: 1 ms (96.37%), Space: 44.1 MB (22.29%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int result[]=new int[nums.length];
+        int index=0;
+        for(int i=0;i<nums.length-1;i++){
+          if(nums[i]==nums[i+1]){
+            nums[i]*=2;
+            nums[i+1]=0;
+          }
+      }
+     for(int i=0;i<nums.length;i++){
+          if(nums[i]!=0)
+           result[index++]=nums[i];
+        }
+        
+        
+        return result;
+    }
+}
