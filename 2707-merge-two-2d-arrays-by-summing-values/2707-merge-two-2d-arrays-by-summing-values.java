8bd26412fba8d767399e2e717a4de9685d572a4f
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-03-02 11:11:14 +0530

    Time: 5 ms (28.44%), Space: 45.6 MB (31.44%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+       
+
+        Map<Integer,Integer> map=new HashMap<>();
+
+         for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i][0],nums1[i][1]);
+            
+        }
+
+         for(int i=0;i<nums2.length;i++){
+          
+                     if(map.containsKey(nums2[i][0])){
+                      int t= map.get(nums2[i][0]);
+                       map.put(nums2[i][0],nums2[i][1]+t);
+                     }
+                     else{
+                        map.put(nums2[i][0],nums2[i][1]);
+                     }
+            
+        }
+Map<Integer, Integer> sortedMap = new TreeMap<>(map);
+        int[][] array = convertMapTo2DArray(sortedMap);
+
+
+        return array;
+    }
+     public static int[][] convertMapTo2DArray(Map<Integer, Integer> map) {
+        int[][] array = new int[map.size()][2]; // Create 2D array
+        int index = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            array[index][0] = entry.getKey();   // Store key in column 0
+            array[index][1] = entry.getValue(); // Store value in column 1
+            index++;
+        }
+
+        return array;
+    }
+}
