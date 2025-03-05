b28a57a6d4260bebac9eeaf84ee837ace2237668
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-03-05 17:49:12 +0530

    Time: 0 ms (100%), Space: 42.7 MB (16.35%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+           int goodEmploy=0;
+       for(int i=0;i<hours.length;i++) {
+        if(hours[i]>= target){
+            goodEmploy++;
+
+
+        }
+       }
+        return   goodEmploy;
+
+    }
+}
