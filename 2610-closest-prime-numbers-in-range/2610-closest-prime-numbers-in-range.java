d732fcacfb79f81ee8ee8be5833169d6e38ed265
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-03-07 15:04:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -18,11 +18,8 @@ class Solution {
     }
 }
         int[] arr = prime.stream().mapToInt(Integer::intValue).toArray();
-          int  result[]=new int[2];
         if(arr.length<2){
-            result[0]=-1;
-            result[1]=-1;
-         return result;
+            
         }
         int ans[]=new int[arr.length-1];
         for(int i=0;i<ans.length;i++){
@@ -43,7 +40,7 @@ class Solution {
            }
              }
 
- 
+   int  result[]=new int[2];
       if(ansindex==-1){
             result[0]=-1;
             result[1]=-1;
