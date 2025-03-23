569a1a00060d9bc0365ef4c37ad49909dccb6af6
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-03-23 13:17:32 +0530

    Time: 1 ms (58.98%), Space: 42.1 MB (16.31%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        char jew[]=jewels.toCharArray();
+       /* char jew[]=jewels.toCharArray();
         char stone[]=stones.toCharArray();
         int jewCount=0;
         for(int i=0;i<jew.length;i++){
@@ -10,6 +10,16 @@ class Solution {
             }
            }
         }
-     return   jewCount;
+     return   jewCount;*/
+
+     
+        int count = 0;
+        Set set = new HashSet<>();
+     for(int i: jewels.toCharArray())
+         set.add(i);
+     for(int j: stones.toCharArray())
+         if(set.contains(j)) count++;
+        return count;
+    
     }
 }
