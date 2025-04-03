ee034d32d6d9719e90efeb311fc3132b0d7a2218
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-04-03 14:47:10 +0530

    Time: 0 ms (100%), Space: 41.5 MB (51.41%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -26,15 +26,6 @@ public class MyQueue {
     return input.empty() && output.empty();
   }
 
-  public static void main(String[] args) {
-    MyQueue queue = new MyQueue();
-    queue.push(1);
-    queue.push(2);
-    System.out.println(queue.peek());  // returns 1
-    System.out.println(queue.pop());   // returns 1
-    System.out.println(queue.empty()); // returns false
-  }
-
 }
 
 /**
