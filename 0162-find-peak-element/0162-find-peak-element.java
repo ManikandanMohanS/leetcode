3437e57c59f5daa4f133f5e26757a903641b82bf
Manikandan Mohan <manikandanmcse2023@ksrce.ac.in>
2025-04-17 21:21:31 +0530

    Time: 0 ms (100%), Space: 42.5 MB (56.5%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -17,7 +17,7 @@ class Solution {
               if(nums[i-1]<nums[i] && nums[i+1]<nums[i] ){
                 if(peak<nums[i]){
                     peak=nums[i];
-                    peakInd=i;
+                    return i;
                 }
               }
         }
