ef2ae0cc5bddfa44bd962b906d53668882d0ac5f
Manikandan Mohan <manikandan862004@gmail.com>
2025-08-08 18:11:45 +0530

    Time: 1 ms (99.85%), Space: 46.5 MB (25.92%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        //Arrays.sort(nums);
+        int xor=0;
+      for(int i=0;i<nums.length;i++)
+      {
+    xor=xor^nums[i];
+      }
+
+    return xor;
+    }
+}
